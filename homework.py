992d669340fba19581256bc5ac06d9515f20c04d
Olha <orukshyna@gmail.com>
2021-07-05 16:43:29 +0200

    Defined main function, moved initialization of the functions to main function

--- homework.py
+++ homework.py
@@ -88,19 +88,21 @@ class CaloriesCalculator(Calculator):
             return 'Хватит есть!'
 
 
-# создадим калькулятор денег с дневным лимитом 1000
-cash_calculator = CashCalculator(1000)
+if __name__ == "__main__":
+    # создадим калькулятор денег с дневным лимитом 1000
+    cash_calculator = CashCalculator(1000)
 
-# дата в параметрах не указана,
-# так что по умолчанию к записи
-# должна автоматически добавиться сегодняшняя дата
-cash_calculator.add_record(Record(amount=145, comment='кофе'))
-# и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
-# а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(amount=3000,
-                                  comment='бар в Танин др', date='08.11.2019'))
+    # дата в параметрах не указана,
+    # так что по умолчанию к записи
+    # должна автоматически добавиться сегодняшняя дата
+    cash_calculator.add_record(Record(amount=145, comment='кофе'))
+    # и к этой записи тоже дата должна добавиться автоматически
+    cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
+    # а тут пользователь указал дату, сохраняем её
+    cash_calculator.add_record(Record(amount=3000,
+                                      comment='бар в Танин др',
+                                      date='08.11.2019'))
 
-print(cash_calculator.get_today_cash_remained('eur'))
-# должно напечататься
-# На сегодня осталось 555 руб
+    print(cash_calculator.get_today_cash_remained('eur'))
+    # должно напечататься
+    # На сегодня осталось 6.53 Euro
